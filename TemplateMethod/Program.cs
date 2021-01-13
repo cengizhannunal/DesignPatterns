4bd430a75c209d3884d87d5e59b9ee6ccd93b5f6
cengizhan ünal <cengizhan.unal@hotmail.com>
2021-01-13 22:47:17 +0300

    Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            FileLogger fileLogger = new FileLogger();
+            fileLogger.Log("Mesaj dosyaya loglandı.");
+
+            DatabaseLogger databaseLogger = new DatabaseLogger();
+            databaseLogger.Log("Mesaj veritabanına loglandı.");
+
+            Console.ReadKey();
+        }
+
+
+        abstract class Logger
+        {
+            protected abstract void Logging(string message);
+            public void Log(string message)
+            {
+                Logging(message);
+            }
+        }
+
+        class FileLogger : Logger
+        {
+            protected override void Logging(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        class DatabaseLogger : Logger
+        {
+            protected override void Logging(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
