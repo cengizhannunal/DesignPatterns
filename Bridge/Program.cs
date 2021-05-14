b03df4c4d5163d277133b89db9b0eee9bdb7ff72
cengizhan ünal <cengizhan.unal@hotmail.com>
2021-05-14 20:29:48 +0300

    Bridge Pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductRepository productRepository = new ProductRepository(new SqlDbProvider());
+
+            productRepository.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IDbContext
+    {
+        public void OpenConnection();
+        public void CloseConnection();
+
+    }
+
+    //Bridge
+    public abstract class DbContext : IDbContext
+    {
+        readonly IDbContext _dbContext;
+
+        protected DbContext(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public virtual void CloseConnection()
+        {
+            _dbContext.CloseConnection();
+        }
+
+        public virtual void OpenConnection()
+        {
+            _dbContext.OpenConnection();
+        }
+    }
+
+    public class SqlDbProvider : IDbContext
+    {
+        public void CloseConnection()
+        {
+            Console.WriteLine("Sql close");
+        }
+
+        public void OpenConnection()
+        {
+            Console.WriteLine("Sql open");
+        }
+    }
+
+    public class OracleProvider : IDbContext
+    {
+        public void CloseConnection()
+        {
+            Console.WriteLine("Oracle close");
+        }
+
+        public void OpenConnection()
+        {
+            Console.WriteLine("Oracle open");
+        }
+    }
+
+
+    public class ProductRepository : DbContext
+    {
+        public ProductRepository(IDbContext dbContext) : base(dbContext)
+        {
+        }
+        public void Save()
+        {
+            OpenConnection();
+
+            //save
+            Console.WriteLine("Ürün eklendi.");
+
+            CloseConnection();
+        }
+    }
+}
