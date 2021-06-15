5e7de0eb8e15d9005802dd76ca050b1dc900fa3d
cengizhan ünal <cengizhan.unal@hotmail.com>
2021-06-15 16:16:56 +0300

    Visitor Pattern

---
+++ Visitor/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPhone iPhone = new IPhone();
+            iPhone.Accept(new OsVisitor());
+            iPhone.Accept(new StoreVisitor());
+
+            Console.WriteLine("------------");
+
+            Samsung samsung = new Samsung();
+            samsung.Accept(new OsVisitor());
+            samsung.Accept(new StoreVisitor());
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Phone
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public abstract void Accept(Visitor visitor);
+    }
+    class IPhone : Phone
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Generate(this);
+        }
+    }
+
+    class Samsung : Phone
+    {
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Generate(this);
+        }
+    }
+
+    abstract class Visitor
+    {
+        public abstract void Generate(Phone phone);
+    }
+
+
+    class OsVisitor : Visitor
+    {
+        public override void Generate(Phone phone)
+        {
+            if (phone is IPhone)
+            {
+                Console.WriteLine("Telefona Ios işletim sistemi yüklendi.");
+            }
+            else if (phone is Samsung)
+            {
+                Console.WriteLine("Telefona Android işletim sistemi yüklendi.");
+            }
+        }
+    }
+
+    class StoreVisitor : Visitor
+    {
+        public override void Generate(Phone phone)
+        {
+            if (phone is IPhone)
+            {
+                Console.WriteLine("Telefona App Store market yüklendi.");
+            }
+            else if (phone is Samsung)
+            {
+                Console.WriteLine("Telefona Google Play market yüklendi.");
+            }
+        }
+    }
+
+}
